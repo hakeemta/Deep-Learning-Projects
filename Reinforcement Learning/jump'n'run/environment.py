a8921443a8957fbaff2ffe3a111ea6464b76bee8
Sophia <lawalsophia@gmail.com>
2023-01-19 12:45:23 +0100

    docs: spell check and corrections

--- Reinforcement Learning/jump'n'run/environment.py
+++ Reinforcement Learning/jump'n'run/environment.py
@@ -5,11 +5,11 @@ from gymnasium.spaces import Discrete
 
 class GameEnv(gym.Env):
     """ A gym environment for a simplistic game world 
-        in which the enironment dynamics (i.e state transition and reward)
+        in which the environment dynamics (i.e state transition and reward)
         is known.
 
     Args:
-    
+
         transitions (list): list of transition probability matrices (np.array)
         rewards (list): state-action rewards
     """
@@ -17,16 +17,16 @@ class GameEnv(gym.Env):
     def __init__(self, transitions, rewards):
         self.transitions = transitions
         self.rewards = rewards
-        
+
         self.observation_space = Discrete(self.transitions.shape[1])
         self.action_space = Discrete(self.rewards.shape[1])
 
         self.current_state = None
         self.total_reward = None
-        self.terminal_state = None
+        self.terminal_state = 4
 
     def step(self, action):
-        """Run one timestep of the environment's dynamics. When end of
+        """Run one time-step of the environment's dynamics. When end of
         episode is reached, you are responsible for calling `reset()`
         to reset this environment's state.
         Accepts an action and returns a tuple (observation, reward, stop, info).
@@ -41,10 +41,10 @@ class GameEnv(gym.Env):
             truncated (bool): whether the episode was truncated
             info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
         """
-        
+
         length = range(self.transitions.shape[1])
-        # Get the tranisition probabiliteies of the current state given an action
-        probability = self.transitions[action,self.current_state,: ]
+        # Get the transition probabilities of the current state given an action
+        probability = self.transitions[action, self.current_state, :]
         next_state = np.random.choice(length, p=probability)
         # Get the reward of the current state given an action
         reward = self.rewards[self.current_state, action]
@@ -66,7 +66,7 @@ class GameEnv(gym.Env):
         super().reset(seed=seed)
         info = {}
         # implement the environment reset functionality here...
-        self.current_state = 0 # state s0
+        self.current_state = 0  # state s0
         obs = self.current_state
         return obs, info
 
